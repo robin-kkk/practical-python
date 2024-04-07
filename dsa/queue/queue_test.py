c36f979dda6d192fc495bb41935dc564918ed2f2
robin-kkk <gej48443@gmail.com>
2024-04-07 11:57:05 +0900

    add mono deque.

--- dsa/queue/queue_test.py
+++ dsa/queue/queue_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from queue import Queue, ListQueue
+import queue_utils
 
 
 class TestQueue(unittest.TestCase):
@@ -29,6 +30,26 @@ class TestQueue(unittest.TestCase):
         assert queue.dequeue() == "1"
         assert queue.dequeue() == "2"
         assert queue.dequeue() == "4"
+    
+    def test_get_monotonic_increasing_stack(self) -> None:
+        # (arr, expected_result)
+        tc = [
+            ([3, 2, 4, 1, 5, 7], [1, 5, 7]),
+            ([2, 5, 3, 8, 7], [2, 3, 7]),
+        ]
+
+        for arr, expected_result in tc:
+            assert queue_utils.get_monotonic_increasing_deque(arr) == expected_result
+
+    def test_get_monotonic_decreasing_stack(self) -> None:
+        # (arr, expected_result)
+        tc = [
+            ([3, 2, 4, 7, 5, 1], [7, 5, 1]),
+            ([6, 4, 7, 1, 8, 3], [8, 3]),
+        ]
+
+        for arr, expected_result in tc:
+            assert queue_utils.get_monotonic_decreasing_deque(arr) == expected_result
 
 
 if __name__ == "__main__":
--- dsa/queue/queue_utils.py
+++ dsa/queue/queue_utils.py
@@ -1,1 +1,19 @@
-from queue import Queue
+from collections import deque
+
+
+def get_monotonic_increasing_deque(arr: list) -> list:
+    dq = deque()
+    for x in arr:
+        while dq and dq[-1] > x:
+            dq.pop()
+        dq.append(x)
+    return list(dq)
+
+
+def get_monotonic_decreasing_deque(arr: list) -> list:
+    dq = deque()
+    for x in arr:
+        while dq and dq[-1] < x:
+            dq.pop()
+        dq.append(x)
+    return list(dq)
