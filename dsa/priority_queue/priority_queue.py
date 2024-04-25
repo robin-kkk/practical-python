6ac8c44a3528ef6d4ba7db1c27272e826fb26f7a
robin-kkk <gej48443@gmail.com>
2024-04-25 09:22:14 +0900

    Apply black

--- dsa/priority_queue/priority_queue.py
+++ dsa/priority_queue/priority_queue.py
@@ -25,7 +25,7 @@ class PriorityQueue:
         if len(self._map[obj]) == 0:
             self._map.pop(obj)
         return None
-    
+
     def _swap(self, i: int, j: int) -> None:
         o1, o2 = self._heap[i], self._heap[j]
         self._map[o1].remove(i)
@@ -34,7 +34,7 @@ class PriorityQueue:
         self._map[o2].add(i)
         self._heap[i], self._heap[j] = o2, o1
         return None
-    
+
     def _swim_up(self, at: int) -> None:
         parent = int((at - 1) / 2)
         while at > 0 and self._higher(self._heap[at], self._heap[parent]):
@@ -42,39 +42,39 @@ class PriorityQueue:
             at = parent
             parent = int((at - 1) / 2)
         return None
-    
+
     def _sink_down(self, at: int) -> None:
         while True:
             candidate = at
-            l, r = at*2 + 1, at*2 + 2
-            
+            l, r = at * 2 + 1, at * 2 + 2
+
             # Choose the smallest child node.
             left_child = self._heap[l] if l < self._size else None
             if left_child and self._higher(left_child, self._heap[candidate]):
                 candidate = l
-            
+
             right_child = self._heap[r] if r < self._size else None
             if right_child and self._higher(right_child, self._heap[candidate]):
                 candidate = r
-            
+
             if candidate == at:
                 break
-            
+
             self._swap(candidate, at)
             at = candidate
-            
+
         return None
-    
+
     def peek(self):
         if self.is_empty():
-            raise Exception('PriorityQueue is empty.')
+            raise Exception("PriorityQueue is empty.")
         return self._heap[0]
 
     def poll(self) -> object:
         if self.is_empty():
-            raise Exception('PriorityQueue is empty.')
+            raise Exception("PriorityQueue is empty.")
         return self.remove_at(0)
-        
+
     def add(self, obj: object) -> None:
         # Append it to the end of our heap.
         self._heap.append(obj)
@@ -82,7 +82,7 @@ class PriorityQueue:
         self._add_map(obj, self._size)
         # Swim up the node at the last position.
         self._swim_up(self._size)
-        # Increment the number of nodes. 
+        # Increment the number of nodes.
         self._size += 1
         # Do doubling the capacity if the size exceeds it.
         if self._size > self._capacity:
@@ -90,8 +90,8 @@ class PriorityQueue:
 
     def remove_at(self, at: int) -> object:
         if self.is_empty():
-            raise Exception('PriorityQueue is empty.')
-        
+            raise Exception("PriorityQueue is empty.")
+
         # Decrement the number of nodes.
         self._size -= 1
         # Swap the given node with the last one.
@@ -105,17 +105,17 @@ class PriorityQueue:
         if at == self._size:
             return removed
 
-        # Try sinking down or swimming up. 
+        # Try sinking down or swimming up.
         prev_last_obj = self._heap[at]
         self._sink_down(at)
         if self._heap[at] == prev_last_obj:
             self._swim_up(at)
 
         return removed
-    
+
     def remove(self, obj: object) -> object:
         if self.is_empty():
-            raise Exception('PriorityQueue is empty.')
+            raise Exception("PriorityQueue is empty.")
         # The first position where a given object occurs.
         at = self._map[obj].pop()
         # Restore it to make removal operation correct.
@@ -136,11 +136,11 @@ class PriorityQueue:
         self._capacity = 0
         self._heap.clear()
         self._map.clear()
-    
+
     def validate_heap_property(self, i: int) -> bool:
         if i >= self._size:
             return True
-        l, r = i*2 + 1, i*2 + 2
+        l, r = i * 2 + 1, i * 2 + 2
         if l < self._size and self._higher(self._heap[l], self._heap[i]):
             return False
         if r < self._size and self._higher(self._heap[r], self._heap[i]):
